4013ce8c6c12eb2abc21fa905c662849c3ca623a
llaurea <laurasguimaraes.2017@gmail.com>
2026-05-12 10:32:52 -0300

    Sprites 4 direções
    Jeca andando nas 4 direções principais. Sprite provisório

--- spellnsteam/scenes/player.gd
+++ spellnsteam/scenes/player.gd
@@ -1,7 +1,27 @@
 extends Area2D
 
-@export var speed := 600.0
+@export var speed := 500.0
+
+@onready var anim = $AnimatedSprite2D
 
 func _process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
 	position += direction * speed * delta
+
+	# Animation handling
+	if direction == Vector2.ZERO:
+		anim.stop()
+		return
+
+	# Horizontal movement takes priority
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			anim.play("walkright")
+		else:
+			anim.play("walkleft")
+	else:
+		if direction.y > 0:
+			anim.play("walkfront")
+		else:
+			anim.play("walkback")
