a14b4c5bf059e69a10c2b13bc4579ce012fbcd57
llaurea <laurasguimaraes.2017@gmail.com>
2026-05-26 15:18:44 -0300

    nn sei qq eu fiz
    fe

--- spellnsteam/scripts/GameManager.gd
+++ spellnsteam/scripts/GameManager.gd
@@ -1,4 +1,58 @@
 extends Node
 
+#MANAGEMENT DO MAPA
 var next_spawn = ""
 var changing_map = false
+
+#MANAGEMENT DO DIA
+var current_day = 1
+var is_daytime = true
+
+#MANAGEMENT DAS ACOES
+var max_day_actions = 3
+var max_night_actions = 2
+var remaining_actions = 3
+
+#STATS DO PLAYER
+var water = 0
+var comfort = 0
+var popularity = 0
+
+#INVENTARIO PLAYER
+var wood = 0
+var coal = 0
+var shopitem = 0
+
+#SAUNA RIVAL 1
+var rival_water = 2
+var rival_comfort = 2
+var rival_popularity = 2
+
+#SISTEMA DE ACOES
+func spend_action(amount := 1):
+	if remaining_actions < amount:
+		print("NO ACTIONS LEFT")
+		return false
+	remaining_actions -= amount
+	print("Remaining actions:", remaining_actions)
+	return true
+
+#SISTEMA DIA/NOITE
+func end_phase():
+
+	# DAY -> NIGHT
+	if is_daytime:
+
+		is_daytime = false
+		remaining_actions = max_night_actions
+
+		print("Night begins")
+
+	# NIGHT -> NEXT DAY
+	else:
+
+		is_daytime = true
+		current_day += 1
+		remaining_actions = max_day_actions
+
+		print("Day", current_day, "begins")
--- spellnsteam/scripts/teste_transição.gd
+++ spellnsteam/scripts/teste_transição.gd
@@ -3,14 +3,27 @@ extends Area2D
 @export_file("*.tscn") var target_scene
 @export var spawn_name : String
 
+# NEW
+@export var costs_action := false
+
 func _on_body_entered(body):
+
 	if body.name != "Player":
 		return
 
 	if GameManager.changing_map:
 		return
-	GameManager.changing_map = true
 
+	# =========================
+	# ACTION CHECK
+	# =========================
+
+	if costs_action:
+
+		if !GameManager.spend_action():
+			return
+
+	GameManager.changing_map = true
 	GameManager.next_spawn = spawn_name
 
 	get_tree().current_scene.call_deferred(
